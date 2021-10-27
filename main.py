4a949bf2ccff674251277efdfaaca994a87d374b
Gabrielalfaro1341 <90943884+Gabrielalfaro1341@users.noreply.github.com>
2021-10-26 22:26:58 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from eliminar_datos_erroneos import eliminar_datos
 from sumar import suma
 import statsmodels.api as sm
 from calculo_pendiente import calculo_pendiente
+from exponencial_fit import fiteo, func
 
 
 directorio=input()
@@ -11,13 +12,19 @@ nombre = directorio.split('/')
 path=[n+'/' for n in nombre[:-1]]
 direccion=''
 
+# Editar data frame
+
 for i in path:
     direccion+=i
+
 df=pd.read_csv(directorio,delimiter=',')
 fig, (ax,ax2)=plt.subplots(2,1)
 df=eliminar_datos(df)
 df=suma(df)
-ax.plot(df['Time(s)'],df['DeltaP(Deg)'],alpha=1)
+df['DeltaP(Deg)']=df['FaradayVoltage(V)']*13
+# graficar data frame
+
+ax.scatter(df['Time(s)'],df['DeltaP(Deg)'],alpha=0.015)
 ax.set_ylabel('Delta P°')
 ax2.set_xlabel('Tiempo (s)')
 ax.set_title('Crecimiento de pelicula para '+nombre[-1].replace('.txt',''))
@@ -25,17 +32,22 @@ calculo_pendiente(df,ax)
 ax2.plot(df['Time(s)'],df['StraylightVoltage(V)']*1000,alpha=0.5)
 fig.subplots_adjust(hspace=0)
 ax2.set_ylabel('StraylightVoltage(mV)')
-ax2.xaxis.grid()
-ax.xaxis.grid()
+ax2.xaxis.grid(linestyle='--')
+ax.xaxis.grid(linestyle='--')
+ax.legend()
 
 # suavizado straylight
+
 lowes1 = sm.nonparametric.lowess(df.loc[:, 'StraylightVoltage(V)']*1000,
                                      df.loc[:, 'Time(s)'], frac=0.1)
 
 ax2.plot(lowes1[:, 0], lowes1[:, 1], color='blue')
 
+fiteo(df,ax,func)
+
 
 
 
 plt.savefig(direccion+nombre[-1].replace('.txt', '.png'),dpi=300)
+
 plt.show()
